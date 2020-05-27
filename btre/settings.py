5d06f060d61ed8e1bdbee22c9957f1cababc365c
avborik <avborik28@gmail.com>
2020-05-27 22:49:52 +1200

    Added import to production settings

--- btre/settings.py
+++ btre/settings.py
@@ -145,13 +145,6 @@ MESSAGE_TAGS = {
     
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='avborik28@gmail.com'
-EMAIL_HOST_PASSWORD='gsmhuxootrvmtzjh'
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
