edb5de367b95baf87b135106831372b9cc428f00
avborik <avborik28@gmail.com>
2020-05-28 22:48:51 +1200

    sdzfdfg

--- btre/settings.py
+++ btre/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    # os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Media folder settings
