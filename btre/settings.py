33b53972b5c88e2f9f4b7638f83b8bc1930ee265
avborik <avborik28@gmail.com>
2020-05-28 22:12:13 +1200

    XCVXCV

--- btre/settings.py
+++ btre/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
-
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
