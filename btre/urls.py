0bd062e52fb9961a2870c45d792e666fa5858379
avborik <avborik28@gmail.com>
2020-05-31 00:13:07 +1200

    KJJ

--- btre/urls.py
+++ btre/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
