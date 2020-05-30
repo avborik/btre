0c4a4736e39382fd954300f9e03952766a734e0e
avborik <avborik28@gmail.com>
2020-05-31 00:51:17 +1200

    fdvfg

--- btre/settings.py
+++ btre/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 
 # Media folder settings set diffrent folder
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles/btre/static/media')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.django.whitenoise.storage.CompressedStaticFilesStorage'
